a09b1b0096395144204b5801b750ed1da031f8db
KnotScripts <mouse2023hub@gmail.com>
2025-09-24 23:54:07 +0200

    ConditionFormattedDescriptionPatch for modifying the description of Conditions.

---
+++ Patches/ConditionFormattedDescriptionPatch.cs
@@ -0,0 +1,63 @@
+﻿using EFT.Quests;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Reflection;
+using TaskBender.Helpers;
+using TaskBender.Services;
+
+namespace TaskBender.Patches
+{
+    internal class ConditionFormattedDescriptionPatch : ModulePatch
+    {
+        private static ConditionUpdater conditionUpdater = new ConditionUpdater();
+
+        public static void LogResultDebug(Condition condition, string result)
+        {
+            if (Globals.Debug)
+                LogHelper.LogInfo($"{condition.GetType().Name} result: {result}");
+        }
+
+        [PatchPostfix]
+        public static void PatchPostfix(Condition __instance, ref string __result)
+        {
+            if (__instance == null)
+                return;
+            try
+            {
+                Condition condition = __instance;
+
+                if (condition is ConditionZone)
+                    return;
+
+                condition.DynamicLocale = true;
+                conditionUpdater.Update(ref condition);
+                //HACK two times so the message is correctly modified.
+                __result = condition.FormattedDescription;
+                __result = condition.FormattedDescription;
+                LogResultDebug(__instance, __result);
+            }
+            catch (Exception exception)
+            {
+                LogHelper.LogExceptionToConsole(exception);
+            }
+        }
+
+        [PatchPrefix]
+        public static void PatchPrefix(ref Condition __instance)
+        {
+            if (__instance == null)
+                return;
+        }
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.FirstMethod(typeof(Condition), this.isTargetMethod);
+        }
+
+        private bool isTargetMethod(MethodBase method)
+        {
+            return method.Name == $"get_{nameof(Condition.FormattedDescription)}";
+        }
+    }
+}
